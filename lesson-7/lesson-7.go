8c4f4efc0a52d5fa7a2ba6bc177cea4948b8ba50
nfarah86 <nfarah86@gmail.com>
2018-04-27 13:24:27 -0700

    wrap up type assertions, switch statements, interfaces, and pointers

--- lesson-7/lesson-7.go
+++ lesson-7/lesson-7.go
@@ -14,9 +14,13 @@ type testiface interface {
 
 	SayHello()
 	Say(s string)
+	Increment()
+	GetInternalValue() int
 }
 
-type testConcreteImplementation struct {}
+type testConcreteImplementation struct {
+	i int 
+}
 	// implements the methods the interface supports
 	// receiver syntax 
 
@@ -31,17 +35,97 @@ func (variable testConcreteImplementation)Say(s string) {
 	fmt.Println(s, "Marsha")
 }
 
+func (variable *testConcreteImplementation) Increment() {
+	variable.i++
+}
+
+func (variable *testConcreteImplementation) GetInternalValue() int {
+	return variable.i
+}
+
 // say and sayhello belong to testConcreteImplementation struct type
 
+func NewTestConcreteImplementationConstructor() testiface {
+	return new(testConcreteImplementation)
+	// can also do &testConcreteImplementation{}
+	// returns a pointer
+}
+
+func NewTestConcreteImplementationConstructorWithV(v int) testiface {
+	return &testConcreteImplementation{i:v}
+}
+
+//embedding in go: encapsulate an object with another object
+type testEmbedding struct { // to have all the features of testConcreteImplementation
+	*testConcreteImplementation //embedding
+	// able to access all the methods of testConcreteImplementation with this type
+}
+
+func testEIface(v interface{}) {
+	// this is an empty interface
+	// there are no methods to satisfy
+	// any datatype can go through
+
+	/*
+	RUN THIS AS AN OPTION as a type assertion
+	// executes if v is only int;
+	if i, ok := v.(int); ok {
+		fmt.Println ("I am an integer of value", i)
+	} else {
+		fmt.Println ("I am not an integer type")
+	}
+	*/
+
+	// RUN THIS AS AN OPTION as a type switch statement
+
+	switch val := v.(type) {
+	case int:
+		fmt.Println ("I'm an int ", val)
+	case string:
+		fmt.Println("I'm a string",val)
+	default:
+		fmt.Println("I'm not a string or int", val)
+	}
+}
 
 func main() {
 	var tiface testiface
-	tiface = testConcreteImplementation{} // called struct literal 
+	
+	// *************** one way to write the pointer is here: ***************
+	// tiface = &testConcreteImplementation{} // called struct literal 
+	// & memory address of value, which is a reference
+	// can also used new(testConcreteImplementation) istead of &
+	
+	// *************** another way to write the pointer is here: ***************
+	// tiface = NewTestConcreteImplementationConstructor()
+
+
+	// *************** another way to write the pointer is here: ***************
+	tiface = NewTestConcreteImplementationConstructorWithV(5)
 	tiface.SayHello()
 	tiface.Say("Hello again!")
+	tiface.Increment()
+	tiface.Increment()
+	fmt.Println(tiface.GetInternalValue())
+	// when implemented without the * and &, you get 0. Why? it's not a pointer. The types of the receivers are not a pointer.
+	// The original object doesn't retain changes that methods do to it
+	// to fix, use pointer -> get passed by reference instead of value.
+	// used heavily; can pass any value type, including structs, interfaces
+	fmt.Println ( "Embedding example")
+	te :=testEmbedding{testConcreteImplementation: &testConcreteImplementation{i:50}}
+	te.SayHello()
+	te.Say("Hola")
+	te.Increment()
+	fmt.Println(te.GetInternalValue())
+
+	fmt.Println("********** empty interface **********")
+	testEIface(3) // no error can pass any object
+	testEIface("this is a string")
+	testEIface(tiface)
+
+	// if you want to specify the datatypes passed in the interface
+	// can use type assertion
 }
-
-
 
 // interface is a type in go
 // can envoke interfaces 
